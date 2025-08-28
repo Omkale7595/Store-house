05ba48ea1951b93352454459e305b09e66a40f83
Om kale <rebootmemory196@gmail.com>
2025-08-28 10:50:10 +0530

    Identical_items and merge_dictionaries
    One is of set and other is of dictionary. In python also set is like maths.

---
+++ identical_items.py
@@ -0,0 +1,4 @@
+def identical_items(set1,set2):
+	print('The identical items of the set1 and set2 is :',set1 & set2)
+	
+identical_items({1,2,3,4},{3,4,5,6})
---
+++ merge_dictionaries.py
@@ -0,0 +1,6 @@
+def merge_dictionaries(dict1,dict2):
+	dict1.update(dict2)
+	print(dict1)
+	
+	
+merge_dictionaries({'Om':'Kale'},{'id':'34'})
