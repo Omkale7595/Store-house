ddf06237fd576b4015cab60c58baa89b054a9f05
Om kale <rebootmemory196@gmail.com>
2025-11-13 18:52:34 +0530

    Christmas tree 🎄

---
+++ Christmas🎄.py
@@ -0,0 +1,25 @@
+for i in range(1,7):
+	for k in range(13,i,-1):
+		print( end=' ') 
+	for j in range(1,i+1):
+		print('*',end='')
+	print()
+for i in range(1,9):
+	for k in range(12,i,-1):
+		print( end=' ') 
+	for j in range(1,i+2):
+		print('*',end='')
+	print()
+for i in range(1,12):
+	for k in range(11,i,-1):
+		print( end=' ') 
+	for j in range(1,i+3):
+		print('*',end='')
+	print()
+for i in range(1,7):
+	for k in range(13,3,-1):
+		print( end=' ') 
+	for a in range(5):
+		print('***',end='')
+		break
+	print()
